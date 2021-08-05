a336a2cc4e4e1007f1e15d1a6635b9184c46d058
Tristan Wang <aawang99@gmail.com>
2021-08-04 20:50:44 -0700

    feat: add hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "John",
-      email: "johnsmith@example.com"
-    },
-    {
-      id: "2",
-      name: "Bob",
-      email: "thisisfake@email.net"
-    }
-  ]
+  const [contacts, setContacts] = useState([])
   return (
     <div className="ui container">
       <Header />
@@ -26,4 +15,4 @@ function App() {
   )
 }
 
-export default App;
+export default App

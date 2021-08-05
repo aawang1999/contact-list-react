ca143805d135cf0266d75e631170df76277ff934
Tristan Wang <aawang99@gmail.com>
2021-08-04 23:12:22 -0700

    modify: fix bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { uuid } from 'uuidv4'
-import './App.css'
-import Header from './Header'
-import AddContact from './AddContact'
-import ContactList from './ContactList'
+import React, { useState, useEffect } from "react"
+import { uuid } from "uuidv4"
+import "./App.css"
+import Header from "./Header"
+import AddContact from "./AddContact"
+import ContactList from "./ContactList"
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts"
@@ -12,17 +12,18 @@ function App() {
   const addContactHandler = (contact) => {
     console.log(contact)
     setContacts([...contacts, { id: uuid(), ...contact }])
-  }
+  };
 
   const removeContactHandler = (id) => {
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id
     })
+
     setContacts(newContactList)
   }
 
   useEffect(() => {
-    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (retrieveContacts) setContacts(retrieveContacts)
   }, [])
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react"
 
 const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
-        <h2>Contact Manager</h2>
+        <h2>Contacts</h2>
       </div>
     </div>
   )
